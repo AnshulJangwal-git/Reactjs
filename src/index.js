ad695a09034b6fd037ab21be853ad30f89359c72
anshul <anshuljangwal006@gmail.com>
2021-08-03 18:55:00 +0530

    up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; //creation Logic
+import ReactDOM from 'react-dom'; //render logic
+
+
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+    <App />, // ek component..
+  document.getElementById('root') //ek jagah jha us component ko dikhana hai 
 );
 
