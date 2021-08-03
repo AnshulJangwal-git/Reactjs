804375bbc41a8f610d8b3622c0b4d024e9d08315
anshul <anshuljangwal006@gmail.com>
2021-08-03 21:33:41 +0530

    React

--- src/index.js
+++ src/index.js
@@ -2,23 +2,25 @@ import React from 'react'; //creation Logic
 import ReactDOM from 'react-dom'; //render logic
 
 
-// import App from './App';
-
-// ReactDOM.render(
-
-//     <App />, // ek component..
-//   document.getElementById('root') //ek jagah jha us component ko dikhana hai 
-// );
-
-import MyComp from "./MyComp" ;
+import App from './App';
 
 ReactDOM.render(
-  <div>
-    <MyComp/>
-    <MyComp/>
-    <MyComp/>
-    <MyComp/>
 
-  </div>, // ek component
-  document.querySelector("#root") // aur ek jagah jha us component ko dikhana hai
+    <App />, // ek component..
+  document.getElementById('root') //ek jagah jha us component ko dikhana hai 
 );
+
+// import MyComp from "./MyComp" ;
+
+// ReactDOM.render(
+//   <div>
+//     <MyComp/>
+//     <MyComp/>
+//     <MyComp/>
+//     <MyComp/>
+//     <MyComp/>
+   
+
+//   </div>, // ek component
+//   document.querySelector("#root") // aur ek jagah jha us component ko dikhana hai
+// );
